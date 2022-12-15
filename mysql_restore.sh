6862de57f930121bd6560e3b525f398d696a507c
tovmayor <tov_mayor@mail.ru>
2022-12-15 18:54:57 +0300

    find 1st try

--- mysql_restore.sh
+++ mysql_restore.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
-# adding client config file  with mesql username & password /home/admin/my_sql.cnf like:
-#[client]
-#user = "some"
-#password = "very complex"
-
-
-#!/bin/bash
 MYSQL='mysql --defaults-extra-file=/root/.my.cnf'
 DB='my_wiki'
+BACKUP_PATH='/root/backup'
 
-for s in `ls /root/backup/$DB`;
+for s in `ls $BACKUP_PATH/$DB`;
     do
-        echo "Import $s"
-        $MYSQL DB < $s;
+        gunzip $BACKUP_PATH/$DB/$s;
+        echo "Import $s";
+        $MYSQL $DB < $BACKUP_PATH/$DB/$s;
 
 done
 
-
-for s in `ls /root/backup/$DB`;
-    do
-        #t=`awk -F. '{print $2}'`;
-        #echo -e "Import table $t\n";
-        $MYSQL DB < $s;
-        done
-
-done
