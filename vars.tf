8115c35a0b50f78b74685b8130466ce6287309d5
tovmayor <tov_mayor@mail.ru>
2023-01-10 15:46:58 +0300

    vmid 17x

--- vars.tf
+++ vars.tf
@@ -1,42 +1,42 @@
 variable "virtual_machines" {
     default = {
         "www" = {
-            vmid = "151"
+            vmid = "171"
             hostname = "www"
             memory = "1024",
             hdd_size = "7G",
             ip_address = "192.168.82.51/24"
         },
         "wiki1" = {
-            vmid = "152"
+            vmid = "172"
             hostname = "wiki1"
             memory = "2048",
             hdd_size = "10G",
             ip_address = "192.168.82.52/24"
         },
         "wiki2" = {
-            vmid = "153"
+            vmid = "173"
             hostname = "wiki2"
             memory = "2048",
             hdd_size = "10G",
             ip_address = "192.168.82.53/24"
         },
         "mysqlmaster" = {
-            vmid = "154"
+            vmid = "174"
             hostname = "mysqlmaster"
             memory = "2048",
             hdd_size = "10G",
             ip_address = "192.168.82.54/24"               
         },  
         "mysqlslave" = {
-            vmid = "155"
+            vmid = "175"
             hostname = "mysqlslave"
             memory = "2048",
             hdd_size = "10G",
             ip_address = "192.168.82.55/24"          
         },          
         "observer" = {
-            vmid = "156"
+            vmid = "176"
             hostname = "observer"
             memory = "4096",
             hdd_size = "15G",
